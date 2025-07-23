5dfec4ce2d63b56656dd3b1ac34fe33a92701422
itismaruf <marufidrs@gmail.com>
2025-07-23 19:54:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,4 +104,4 @@ user_encoded = encoder.transform(user_input)
 for col in ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']:
     user_encoded[col] = user_input[col].values
 
-user_encoded = user_encoded[X_train_encoded.columns]
+user_encoded = encoder.transform.(user_input)
